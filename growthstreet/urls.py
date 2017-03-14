4f55cfa370c13b73cff00c9b6cb09bb22b35d6aa
MichaelWooster <mikewooster@btinternet.com>
2017-03-14 20:05:58 +0000

    added new app - borrowing and added initial view to make sure it is working updated urls in both url-configs to allow access to /borrowing added *.sqlite3 to .gitignore file

--- growthstreet/urls.py
+++ growthstreet/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'borrowing/', include('borrowing.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
