43bd85f63a00b1e459835c814f76ad0f2d952155
MichaelWooster <mikewooster@btinternet.com>
2017-03-15 20:18:52 +0000

    added borrowing models to admin

--- borrowing/admin.py
+++ borrowing/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import User, Company, LoanRequest
+
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Company)
+admin.site.register(LoanRequest)
