0bc80ea4d73eb1eb5f953b7163921459db132c59
MichaelWooster <mikewooster@btinternet.com>
2017-03-15 21:49:42 +0000

    added model forms, the initial template and view to process data

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import UserForm, CompanyForm, LoanRequestForm
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Test text - this render works")
+
+    userform = UserForm()
+    companyform = CompanyForm()
+    loanrequestform = LoanRequestForm()
+    if request.method == "POST":
+        userform = UserForm(request.POST)
+        companyform = CompanyForm(request.POST)
+        loanrequestform = LoanRequestForm(request.POST)
+        if (userform.is_valid() and companyform.is_valid()
+                and loanrequestform.is_valid()):
+            # Process the form data here and commit to db
+            pass
+
+    forms = {"userform": userform,
+             "companyform": companyform,
+             "loanrequestform": loanrequestform}
+    return render(request, "borrowing/index.html", forms)
