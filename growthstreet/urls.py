049fc5c78ce8303cdba806bf90fff09f050b6266
MichaelWooster <mikewooster@btinternet.com>
2017-03-17 18:52:58 +0000

    added redirection from main index to the borrowing page

--- growthstreet/urls.py
+++ growthstreet/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from borrowing import views
+
 urlpatterns = [
-    url(r'borrowing/', include('borrowing.urls')),
+    url(r'^$', views.redirect_to_borrowing),
+    url(r'^borrowing/', include('borrowing.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
