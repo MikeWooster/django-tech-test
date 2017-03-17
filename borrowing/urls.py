09aaf11e557b0d91bc39fc52c7384d324cbecd65
MichaelWooster <mikewooster@btinternet.com>
2017-03-17 18:53:50 +0000

    updated name of borrowing page to borrowingindex to avoid any confusion with any future index page

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index")
+    url(r'^$', views.index, name="borrowingindex"),
+    url(r'summary/(?P<user_id>[0-9]+)/$', views.summary, name="summary"),
 ]
